2caa1e4d3a986bb85bf738b73760a5bbef9bc9ef
Nahida Akther <ummenahida20@gmail.com>
2026-05-02 23:46:14 +0600

    add multiple section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Shared/Header";
 import { Hero } from "@/components/Shared/Hero";
 import { About } from "@/components/Shared/About";
+import { Features } from "@/components/Shared/Features";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
+      <Features />
     </>
   );
 }
