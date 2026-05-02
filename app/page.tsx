1161d255c6be333f60fb7a146e2326ea897e141a
Nahida Akther <ummenahida20@gmail.com>
2026-05-03 00:33:07 +0600

    feat: add core features sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Header } from "@/components/Shared/Header";
 import { Hero } from "@/components/Shared/Hero";
 import { About } from "@/components/Shared/About";
 import { Features } from "@/components/Shared/Features";
+import { DemoVideo } from "@/components/Shared/DemoVideo";
+import { CoreFeatures } from "@/components/Shared/CoreFeatures";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Hero />
       <About />
       <Features />
+      <DemoVideo />
+      <CoreFeatures />
     </>
   );
 }
