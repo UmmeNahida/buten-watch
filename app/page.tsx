82d1f2a270f85305082cab19bfe3b88e25a5035d
Nahida Akther <ummenahida20@gmail.com>
2026-05-02 23:37:41 +0600

    feat(home): Add About section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Shared/Header";
 import { Hero } from "@/components/Shared/Hero";
+import { About } from "@/components/Shared/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
